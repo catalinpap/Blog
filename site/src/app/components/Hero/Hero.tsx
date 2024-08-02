01cf428807253d9ed9fcc979794ae965b2d85c5c
Catalin Pap <catalinpap@Catalins-MacBook-Pro.local>
2024-08-02 23:59:16 +0300

    add dumb controllers to hero section carousel

--- site/src/app/components/Hero/Hero.tsx
+++ site/src/app/components/Hero/Hero.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { IoMdArrowForward, IoMdArrowBack  } from "react-icons/io";
 import "./Hero.css";
 
 const Hero = () => {
@@ -10,9 +11,26 @@ const Hero = () => {
                 height={600}
                 className="hero-bg"
                 />
-            <div className="main-container w-1/3 mb-40">
+            <div className="main-container relative w-1/3 pb-40">
+                {/* Category tag */}
                 <span className="category-tag">Interior Design</span>
+                {/* Title */}
                 <h1 className="hero-title">Design is a plan for arranging elements in such a way as best to accomplish a particular purpose.</h1>
+                {/* Arrows */}
+                <div className="absolute flex bottom-0 right-0">
+                    <span className="arrow">
+                        <IoMdArrowBack size={"full"}/>
+                    </span>
+                    <span className="arrow">
+                        <IoMdArrowForward size={"full"}/>
+                    </span>
+                </div>
+                {/* Bullets */}
+                <div className="flex absolute bottom-0 left-0 space-x-2 mb-8">
+                    <span className="bullet selected"/>
+                    <span className="bullet"/>
+                    <span className="bullet"/>
+                </div>
             </div>
         </section>
     )
